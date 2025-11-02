3025280baae3e230e0a49e3368fc9b5a818db120
gkullio <105679126+gkullio@users.noreply.github.com>
2025-11-02 10:19:40 -0800

    completing changes

--- network.tf
+++ network.tf
@@ -14,30 +14,12 @@ resource "azurerm_subnet" "mgmt" {
   address_prefixes     = [var.mgmt_address_space]
 }
 
-resource "azurerm_subnet" "ext" {
-  name                 = var.ext_subnet_name
-  resource_group_name  = var.rg_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.ext_address_space]
-}
-
 resource "azurerm_subnet" "int" {
   name                 = var.int_subnet_name
   resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.int_address_space]
 }
-
-# # Reference kulland-dns resource group
-# data "azurerm_resource_group" "kulland_dns" {
-#   name     = "kulland-dns"
-# }
-
-# # Reference kulland-dns zone
-# data "azurerm_dns_zone" "zone" {
-#   name                = "kulland.info"
-#   resource_group_name = data.azurerm_resource_group.kulland_dns.name
-# }
 
 # Create public IPs
 resource "azurerm_public_ip" "management_pubip" {
@@ -52,105 +34,6 @@ resource "azurerm_public_ip" "management_pubip" {
     email = var.resourceOwnerEmail
   }  
 }
-
-resource "azurerm_public_ip" "plus1" {
-  name                = "ubuntu-plus1_pubip"
-  location            = var.location
-  resource_group_name = var.rg_name
-  allocation_method   = "Static"   # Static is required due to the use of the Standard sku
-  sku                 = "Standard" # the Standard sku is required due to the use of availability zones
-  depends_on = [ azurerm_resource_group.rg ]
-  tags = {
-    owner = var.resourceOwner
-    email = var.resourceOwnerEmail
-  }  
-}
-
-resource "azurerm_public_ip" "plus2" {
-  name                = "ubuntu-plus2_pubip"
-  location            = var.location
-  resource_group_name = var.rg_name
-  allocation_method   = "Static"   # Static is required due to the use of the Standard sku
-  sku                 = "Standard" # the Standard sku is required due to the use of availability zones
-  depends_on = [ azurerm_resource_group.rg ]
-  tags = {
-    owner = var.resourceOwner
-    email = var.resourceOwnerEmail
-  }
-}
-
-resource "azurerm_public_ip" "plus3" {
-  name                = "ubuntu-plus3_pubip"
-  location            = var.location
-  resource_group_name = var.rg_name
-  allocation_method   = "Static"   # Static is required due to the use of the Standard sku
-  sku                 = "Standard" # the Standard sku is required due to the use of availability zones
-  depends_on = [ azurerm_resource_group.rg ]
-  tags = {
-    owner = var.resourceOwner
-    email = var.resourceOwnerEmail
-  }  
-}
-
-resource "azurerm_public_ip" "oss1" {
-  name                = "ubuntu-oss1_pubip"
-  location            = var.location
-  resource_group_name = var.rg_name
-  allocation_method   = "Static"   # Static is required due to the use of the Standard sku
-  sku                 = "Standard" # the Standard sku is required due to the use of availability zones
-  depends_on = [ azurerm_resource_group.rg ]
-  tags = {
-    owner = var.resourceOwner
-    email = var.resourceOwnerEmail
-  }  
-}
-
-resource "azurerm_public_ip" "oss2" {
-  name                = "ubuntu-oss2_pubip"
-  location            = var.location
-  resource_group_name = var.rg_name
-  allocation_method   = "Static"   # Static is required due to the use of the Standard sku
-  sku                 = "Standard" # the Standard sku is required due to the use of availability zones
-  depends_on = [ azurerm_resource_group.rg ]
-  tags = {
-    owner = var.resourceOwner
-    email = var.resourceOwnerEmail
-  }
-}
-
-resource "azurerm_public_ip" "oss3" {
-  name                = "ubuntu-oss3_pubip"
-  location            = var.location
-  resource_group_name = var.rg_name
-  allocation_method   = "Static"   # Static is required due to the use of the Standard sku
-  sku                 = "Standard" # the Standard sku is required due to the use of availability zones
-  depends_on = [ azurerm_resource_group.rg ]
-  tags = {
-    owner = var.resourceOwner
-    email = var.resourceOwnerEmail
-  }  
-}
-resource "azurerm_public_ip" "waf-test" {
-  name                = "ubuntu-waf-test_pubip"
-  location            = var.location
-  resource_group_name = var.rg_name
-  allocation_method   = "Static"   # Static is required due to the use of the Standard sku
-  sku                 = "Standard" # the Standard sku is required due to the use of availability zones
-  depends_on = [ azurerm_resource_group.rg ]
-  tags = {
-    owner = var.resourceOwner
-    email = var.resourceOwnerEmail
-  }  
-}
-
-# # Create a DNS A record pointing to the BIG-IP Mgmt Public IP
-# resource "azurerm_dns_a_record" "nginx-ubuntu" {
-#   name                = "nginx-ubuntu"
-#   zone_name           = data.azurerm_dns_zone.zone.name
-#   resource_group_name = data.azurerm_resource_group.kulland_dns.name
-#   ttl                 = 60
-#   records             = [resource.azurerm_public_ip.management_pubip.ip_address]
-# }
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "management_nsg" {
@@ -241,42 +124,36 @@ resource "azurerm_network_interface" "internal_nic" {
     subnet_id                     = azurerm_subnet.int.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "172.20.2.101"
-    public_ip_address_id          = azurerm_public_ip.plus2.id
   }
   ip_configuration {
     name                          = "internal_nic_plus3"
     subnet_id                     = azurerm_subnet.int.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "172.20.2.102"
-    public_ip_address_id          = azurerm_public_ip.plus3.id
   }
   ip_configuration {
     name                          = "internal_nic_oss1"
     subnet_id                     = azurerm_subnet.int.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "172.20.2.103"
-    public_ip_address_id          = azurerm_public_ip.oss1.id
   }
   ip_configuration {
     name                          = "internal_nic_oss2"
     subnet_id                     = azurerm_subnet.int.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "172.20.2.104"
-    public_ip_address_id          = azurerm_public_ip.oss2.id
   }
   ip_configuration {
     name                          = "internal_nic_oss3"
     subnet_id                     = azurerm_subnet.int.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "172.20.2.105"
-    public_ip_address_id          = azurerm_public_ip.oss3.id
   }
   ip_configuration {
     name                          = "internal_nic_waf_test"
     subnet_id                     = azurerm_subnet.int.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "172.20.2.106"
-    public_ip_address_id          = azurerm_public_ip.waf-test.id
   }
   depends_on = [ azurerm_resource_group.rg ]
   tags = {
--- ubuntu_outputs.tf
+++ ubuntu_outputs.tf
@@ -1,22 +1,19 @@
 output "management_public_ip_address" {
   value = azurerm_linux_virtual_machine.kulland_ubuntu_vm.public_ip_address
 }
-# output "management_public_ip_address_different_resource" {
-#   value = azurerm_public_ip.management_pubip.ip_address
-# }
-# output "dns_a_record_fqdn" {
-#   value = trimsuffix(azurerm_dns_a_record.nginx-ubuntu.fqdn, ".")
-# }
 output "ssh_command" {
   value = "ssh ${var.ubuntu-username}@${azurerm_linux_virtual_machine.kulland_ubuntu_vm.public_ip_address} -i ${var.ssh_key}"
 }
 output "nginx_instances_public_ip_addresses" {
+  # Only include entries for IPs that actually have an assigned address
   value = {
-    plus1_public_ip                         = azurerm_public_ip.plus1.ip_address
-    plus2_public_ip                         = azurerm_public_ip.plus2.ip_address
-    plus3_public_ip                         = azurerm_public_ip.plus3.ip_address
-    oss1_public_ip                          = azurerm_public_ip.oss1.ip_address
-    oss2_public_ip                          = azurerm_public_ip.oss2.ip_address
-    oss3_public_ip                          = azurerm_public_ip.oss3.ip_address
+    for k, v in {
+      plus1_public_ip = azurerm_public_ip.plus1.ip_address
+      plus2_public_ip = azurerm_public_ip.plus2.ip_address
+      plus3_public_ip = azurerm_public_ip.plus3.ip_address
+      oss1_public_ip  = azurerm_public_ip.oss1.ip_address
+      oss2_public_ip  = azurerm_public_ip.oss2.ip_address
+      oss3_public_ip  = azurerm_public_ip.oss3.ip_address
+    } : k => v if v != null && v != ""
   }
 }
